9256bb0ece5fbb47bea9ae281892e96ae6246c31
marusasa <sasagumaru@gmail.com>
2022-11-24 22:39:57 -0800

    Adding high score

--- js/snake.js
+++ js/snake.js
@@ -1,3 +1,77 @@
+class MarudotHS {
+    constructor(gameId, domElemId){
+        this.gameId = gameId;
+        this.domElemId = domElemId;
+        this.urlBase = 'http://localhost:8080';
+        this.bottomScore = 0;
+        this.hsList = [];
+        this.scoreToKeep = 0;
+    }
+    addScore(score, name){
+        let reqBody = {
+            "gameId": this.gameId,
+            "name": name,
+            "score": score
+        }
+        let obj = {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+            }, 
+            body: JSON.stringify(reqBody)
+        };
+        fetch(this.urlBase + '/addscore',obj)
+            .then((response) => response.json())
+            .then((data) => {
+                if(data.success){
+                   this.hsList = data.scores;
+                   this.scoreToKeep = data.score_to_keep;
+                   this.bottomScore = data.bottom_score;
+                   this.render();
+                }else{
+                    console.error(data.errors);
+                }
+            });
+    }
+    reload(){
+        //fetch high score
+        fetch(this.urlBase + '/scores/' + this.gameId)
+            .then((response) => response.json())
+            .then((data) => {
+                if(data.success){
+                    this.hsList = data.scores;
+                    this.scoreToKeep = data.score_to_keep;
+                    this.bottomScore = data.bottom_score;
+                    this.render();
+                }else{
+                    console.error(data.errors);
+                }
+            });
+    }
+    render(){
+        //clear dom children
+        const parent = document.getElementById(this.domElemId);
+        while (parent.firstChild) {
+            parent.firstChild.remove();
+        }
+        //create html for list
+        for(let x = 0;x < this.scoreToKeep;x++){
+            let score = 0;
+            let name = '          ';
+            if(x < this.hsList.length){
+                score = this.hsList[x].score;
+                name  = this.hsList[x].name;
+            }
+            const liChild = document.createElement('li');
+            liChild.textContent = String(x+1).padStart(2,' ') + ' ' + String(score).padStart(4,' ') + ' ' + name.padEnd(10,' ');
+            parent.appendChild(liChild);
+         }
+    }
+    get bottomScore(){
+        return this.bottomScore;
+    }
+}
+
 (function () {
     var config = {
         type: Phaser.AUTO,
@@ -16,6 +90,9 @@
             preload: preload,
             create: create,
             update: update
+        },
+        dom: {
+            createContainer: true
         }
     };
 
@@ -39,14 +116,22 @@
     var scoreText;
     var score = 0;
     var gameText;
+    var highscoregroup;
+    var hs = {
+        list:[],
+        topScore:0,
+        bottomScore:0,
+        scoreTextArray:[],
+        domElement:null
+    }
 
     function preload() {
         this.load.image('snake_body', 'assets/snake_square.png');
         this.load.image('food', 'assets/food_square.png');
+        this.load.html('hs_input', 'assets/highscore_input.html');
     }
 
-    function create() {
-       
+    function create() {       
         //Draw outer lines
         var line1 = this.add.line(0,0,0,60,780,60,0x3a3a3a);
         line1.setOrigin(0,0);
@@ -57,9 +142,9 @@
         var line4 = this.add.line(0,0,780,60,780,570,0x3a3a3a);
         line4.setOrigin(0,0);
 
-        for(var x = 0;x <= MAX_X;x++){
+        for(let x = 0;x <= MAX_X;x++){
             squares.push(new Array());            
-            for(var y = 0;y <= MAX_Y;y++){
+            for(let y = 0;y <= MAX_Y;y++){
                 squares[x].push(this.physics.add.sprite(30 * (x+1)-15, 30 * (y+3)-15, 'snake_body'));
             }
         }
@@ -90,13 +175,126 @@
             nextDirection.push(1);
         });
         cursors.space.on('down', function (event) {
-           //set score to 0
-           if(!go){
-            resetGame();
-            go = true;
-           }
+            //set score to 0
+            if(!go && !hs.domElement.visible){
+                resetGame();
+                go = true;
+                highscoregroup.setVisible(false);
+            }
         });
+
+       
+        const rect = this.add.rectangle(350, 150, 400, 400, 0x808080);
+        rect.setOrigin(0,0);
+        const rect2 = this.add.rectangle(354, 154, 392, 392, 0xffffff);
+        rect2.setOrigin(0,0);
+        let x = 360;
+        let y = 160;
+        let yAdd = 34;
+        var hs01 = this.add.text(x, y, 'HIGH SCORE:', { fontSize: '32px', fill: '#000' });
+        var hs02 = this.add.text(x, y+=yAdd, '01 .... ..........', { fontSize: '32px', fill: '#000' });
+        var hs03 = this.add.text(x, y+=yAdd, '02 .... ..........', { fontSize: '32px', fill: '#000' });
+        var hs04 = this.add.text(x, y+=yAdd, '03 .... ..........', { fontSize: '32px', fill: '#000' });
+        var hs05 = this.add.text(x, y+=yAdd, '04 .... ..........', { fontSize: '32px', fill: '#000' });
+        var hs06 = this.add.text(x, y+=yAdd, '05 .... ..........', { fontSize: '32px', fill: '#000' });
+        var hs07 = this.add.text(x, y+=yAdd, '06 .... ..........', { fontSize: '32px', fill: '#000' });
+        var hs08 = this.add.text(x, y+=yAdd, '07 .... ..........', { fontSize: '32px', fill: '#000' });
+        var hs09 = this.add.text(x, y+=yAdd, '08 .... ..........', { fontSize: '32px', fill: '#000' });
+        var hs10 = this.add.text(x, y+=yAdd, '09 .... ..........', { fontSize: '32px', fill: '#000' });
+        var hs11 = this.add.text(x, y+=yAdd, '10 .... ..........', { fontSize: '32px', fill: '#000' });
+
+        highscoregroup = this.add.group();
+        highscoregroup.add(rect);
+        highscoregroup.add(rect2);
+        highscoregroup.add(hs01);
+        highscoregroup.add(hs02);
+        highscoregroup.add(hs03);
+        highscoregroup.add(hs04);
+        highscoregroup.add(hs05);
+        highscoregroup.add(hs06);
+        highscoregroup.add(hs07);
+        highscoregroup.add(hs08);
+        highscoregroup.add(hs09);
+        highscoregroup.add(hs10);
+        highscoregroup.add(hs11);
+
+        hs.scoreTextArray = new Array(hs02,hs03,hs04,hs05,hs06,hs07,hs08,hs09,hs10,hs11);
+
+         //fetch high score
+         fetch('http://localhost:8080/scores/878e8b4f-8aee-4a49-b981-9d0eac814edf')
+         .then((response) => response.json())
+         .then((data) => {
+             if(data.success){
+                hs.list = data.scores;
+                showHighScores();
+             }
+         });
+
+         //prepare highscore input html
+         hs.domElement = this.add.dom(200,150).createFromCache('hs_input');
+         hs.domElement.setOrigin(0,0);
+         hs.domElement.on('click', function (event) {
+            if (event.target.name === 'submitButton'){
+                let inputName = this.getChildByName('nameField');
+                if (inputName.value !== ''){
+                    //  Turn off the click events
+                    this.removeListener('click');
+                    console.log(inputName.value);
+                    //call service
+                    let reqBody = {
+                        "gameId": "878e8b4f-8aee-4a49-b981-9d0eac814edf",
+                        "name": inputName.value ,
+                        "score": score
+                    }
+                    let obj = {
+                        method: 'POST', 
+                        headers: {
+                            'Content-Type': 'application/json',
+                        }, 
+                        body: JSON.stringify(reqBody)
+                    };
+                    fetch('http://localhost:8080/addscore',obj)
+                        .then((response) => response.json())
+                        .then((data) => {
+                            if(data.success){
+                                hs.list = data.scores;
+                                hs.domElement.setVisible(false);
+                                game.input.keyboard.startListeners();
+                                showHighScores();
+                            }else{
+                                hs.domElement.addListener('click');
+                                alert(data.errors[0]);
+                            }
+                        });
+                }else{
+                    alert('Please enter your name.');
+                }
+            }else if(event.target.name === 'cancelButton'){
+                this.removeListener('click');
+                console.log('HS Canceled');
+                hs.domElement.setVisible(false);
+                game.input.keyboard.startListeners();
+                showHighScores();
+            }
+        });
+        hs.domElement.setVisible(false);        
     }   
+
+    function askHSName(score){
+        hs.domElement.addListener('click');
+        hs.domElement.getChildByID('hs_score_disp').setHTML(score);
+        hs.domElement.setVisible(true);
+        game.input.keyboard.stopListeners();
+    }
+
+    function showHighScores(){
+        for(let x = 0;x < hs.list.length;x++){
+            let score = hs.list[x].score;
+            let name  = hs.list[x].name;
+            hs.scoreTextArray[x].setText(String(x+1).padStart(2,' ') + ' ' + String(score).padStart(4,' ') + ' ' + name.padEnd(10,' '));
+         }
+         highscoregroup.setVisible(true);
+    }
 
     function resetGame(){
         //hide all snake body
@@ -139,7 +337,6 @@
     function newFood(){
         var newX = Math.floor(Math.random() * (MAX_X-1));
         var newY = Math.floor(Math.random() * (MAX_Y-1));
-        console.log(newX + ' ' + newY);
         while(squares[newX][newY].visible){
             newX = Math.floor(Math.random() * (MAX_X-1));
             newY = Math.floor(Math.random() * (MAX_Y-1));
@@ -185,6 +382,8 @@
             if(currentY < 0 || currentX < 0 || currentY > MAX_Y || currentX > MAX_X ||
                     squares[currentX][currentY].visible){
                 go = false;
+                //setTimeout(showHighScores, 1000);
+                askHSName(score);
                 gameText.setText('Game Over');
                 this.time.delayedCall(2000, function(){if(!go){gameText.setText('Hit Space to Restart');}});
             }else{
@@ -200,6 +399,10 @@
                     newFood();
                     if(gameSpeed > 80){
                         gameSpeed -= 1;
+                    }
+                    //show gamespeed for debug.
+                    if(config.physics.arcade.debug){
+                        gameText.setText(gameSpeed);
                     }
                 }            
                 gameTime = 0;
